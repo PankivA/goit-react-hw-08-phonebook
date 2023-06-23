f050903e4410bf2d3c4e0e9a025ffeaa03d14b45
Andrii Pankiv <pankivand@gmail.com>
2023-06-23 14:38:32 +0300

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import PrivateRoute from 'components/PrivateRoute';
+import RestrictedRoute from 'components/RestrictedRoute';
+import { current } from 'redux/auth/auth-operations';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+
+const HomePage = lazy(() => import('pages/Home'));
+const RegisterPage = lazy(() => import('pages/Register'));
+const LoginPage = lazy(() => import('pages/Login'));
+const ContactsPage = lazy(() => import('pages/Contacts'));
+
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(current);
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Route>
+        <Route element={<RestrictedRoute />}>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,64 @@
+import { useDispatch } from 'react-redux';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import { login } from 'redux/auth/auth-operations';
+import css from './LoginForm.module.css';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+export const LoginForm = () => {
+  const [state, setState] = useState(INITIAL_STATE);
+
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      login({
+        email: state.email,
+        password: state.password,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <label className={css.label}>
+        Email
+        <input
+          type="email"
+          name="email"
+          required
+          value={state.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label className={css.label}>
+        Password
+        <input
+          type="password"
+          name="password"
+          required
+          value={state.password}
+          onChange={handleChange}
+        />
+      </label>
+      <Button type="submit" sx={{ mt: 2 }} color="secondary"  variant="outlined">
+        Log In
+      </Button>
+      {/* <button type="submit">Log In</button> */}
+    </form>
+  );
+};
